5b934e2f318dce00c6ecbfbcac2504e44af8a9bd
NAVEENA-1909 <navimdk2006@gmail.com>
2025-03-06 19:46:59 +0530

    Create Numofemp.java

---
+++ Numofemp.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
+        int count=0;
+        int l=hours.length;
+        for(int i=0;i<l;i++){
+            if(hours[i]>=target){
+                count+=1;
+            }
+        }
+        return count;
+    }
+}
